3552ac0a6e8b694fd4ff25debd994c89ff67e62d
YaraTantawy <142018929+YaraTantawy@users.noreply.github.com>
2025-12-12 16:17:12 +0200

    createkg+graph retreival
    20 smthn queries + creating the database same as from MS2
    
    still needs testing

---
+++ embeddings.py
@@ -0,0 +1,11 @@
+class EmbeddingRetriever:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def embed_hotels(self):
+        # generate and store embeddings
+        pass
+
+    def semantic_search(self, text):
+        # embedding + vector index search
+        pass
